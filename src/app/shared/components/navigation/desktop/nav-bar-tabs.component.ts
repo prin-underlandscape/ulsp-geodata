cdb4da9ccd1138eab337db12f2a2d93d39b90273
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2024-02-01 14:32:58 +0100

    Visibilità dei tasti condizionata al ruolo

--- src/app/shared/components/navigation/desktop/nav-bar-tabs.component.ts
+++ src/app/shared/components/navigation/desktop/nav-bar-tabs.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
+import { map } from 'rxjs/operators';
 import { NavBarTabComponent } from './nav-bar-tab.component';
 
 @Component({
@@ -10,6 +11,23 @@ import { NavBarTabComponent } from './nav-bar-tab.component';
   templateUrl: './nav-bar-tabs.component.html',
 })
 export class NavBarTabsComponent {
+  user$ = this.authService.user$;
   isAuthenticated$ = this.authService.isAuthenticated$;
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {};
+  
+  canDownload(roles: string[]) {
+    return roles.includes('contributor') || roles.includes('user') || roles.includes('admin');
+  }
+
+  canUpload(roles: string[]) {
+    return roles.includes('contributor') || roles.includes('admin');
+  }
+  
+  hasProfile(roles: string[]) {
+    return roles.includes('contributor') || roles.includes('user') || roles.includes('admin');
+  }
+  
+  canAdmin(roles: string[]) {
+    return roles.includes('admin');
+  }
 }
